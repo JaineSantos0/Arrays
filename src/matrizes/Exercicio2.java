7e334fb8d92ccd9b840144c6c669a633d1da31fc
Jaine Santos <jainejosiane@gmail.com>
2023-02-13 22:56:26 -0300

    Exercicio 4 finalizado

--- src/matrizes/Exercicio2.java
+++ src/matrizes/Exercicio2.java
@@ -10,27 +10,26 @@ public class Exercicio2 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        float[][] matrizA = new float[4][4];
+        double[][] matrizA = new double[10][4];
 
         System.out.println("Matriz");
         for (int linha = 0; linha < matrizA.length; linha ++) {
-            for (int coluna = 0; coluna < matrizA.length; coluna ++) {
-               float nota = sc.nextInt();
-               matrizA[linha][coluna] = nota;
+            for (int coluna = 0; coluna < matrizA[linha].length; coluna ++) {
+                matrizA[linha][coluna] = sc.nextDouble();
             }
         }
 
-        float[] media = new float[10];
-        float soma = 0;
+        double[] media = new double[10];
+        double soma = 0;
         for (int linha = 0; linha < matrizA.length; linha ++) {
-            for (int coluna = 0; coluna < matrizA.length; coluna ++) {
+            for (int coluna = 0; coluna < matrizA[linha].length; coluna ++) {
                 soma += matrizA[linha][coluna];
             }
-            media[linha] = soma / 4;
+            media[linha] = soma / matrizA[linha].length;
         }
 
-        for (float medias: media) {
-            System.out.println(medias);
+        for (double medias: media) {
+            System.out.printf("%.2f ",medias);
         }
     }
 }
